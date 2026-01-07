695199a313454ca60bc99730b205befa2fd65706
Damjan Stojchev <damjan.stojcev4@gmail.com>
2026-01-07 12:19:50 +0100

    Removed scroll indicator

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -85,23 +85,6 @@ const HeroSection = () => {
             ))}
           </motion.div>
         </div>
-
-        {/* Scroll Indicator */}
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1, duration: 1 }}
-          className="absolute bottom-10 left-1/2 -translate-x-1/2"
-        >
-          <motion.div
-            animate={{ y: [0, 10, 0] }}
-            transition={{ duration: 2, repeat: Infinity }}
-            className="flex flex-col items-center gap-2 text-muted-foreground"
-          >
-            <span className="text-sm">Scroll to explore</span>
-            <ArrowDown className="w-5 h-5" />
-          </motion.div>
-        </motion.div>
       </div>
     </section>
   );
